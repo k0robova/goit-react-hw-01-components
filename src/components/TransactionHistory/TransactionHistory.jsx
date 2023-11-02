099d59fedff18a69489bf440f8628884ff2bcdcc
k0robova <alonakorobova@ukr.net>
2023-11-02 22:49:47 +0200

    update

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,7 +3,7 @@ import css from './TransactionHistory.module.css'
 
 export const TransactionHistory = ({items}) => {
 
-    return (<table className="transaction-history">
+    return (<table className={css.transactionHistory}>
   <thead>
     <tr>
       <th>Type</th>
