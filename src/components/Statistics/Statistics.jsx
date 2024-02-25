c10bc0bb7fbd38f8640c8af39deef362d86e8362
k0robova <alonakorobova@ukr.net>
2024-02-25 22:09:33 +0200

    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,35 +1,35 @@
-
 import PropTypes from 'prop-types';
-import css from './Statistics.module.css'
+import css from './Statistics.module.css';
+import { getRandomHexColor } from 'helpers/getRandomHexColor';
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 export const Statistics = ({ title, stats }) => {
-    return <section className={css.statistics}>
-       {title &&  <h2 className={css.title}>{title}</h2>}        
-        <ul className={css.list}>
-        {stats.map(({id, label, percentage}) => (
-          <li key={id} className={css.item} style={{ backgroundColor: getRandomHexColor() }}>
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            key={id}
+            className={css.item}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
             <span className={css.label}>{label}</span>
             <span className={css.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
-</section>
-}
+    </section>
+  );
+};
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  
+
   stats: PropTypes.arrayOf(
     PropTypes.exact({
       id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
     }).isRequired
- ) }
-
-
+  ),
+};
